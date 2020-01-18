fe147995383d4057fcfc12b40b10f6272fd3e8ed
machuanjia <yanshi0429@126.com>
2020-01-18 20:39:28 +0800

    add

---
+++ src/views/permissions/permissions-app.vue
@@ -0,0 +1,133 @@
+<template>
+  <app-content
+    class="permissions-app"
+    :is-flex-header="true"
+    :is-hright="true"
+    :is-hcenter="true"
+  >
+    <template v-slot:hcenter>
+      <el-input
+        v-model="searchText"
+        class="search-container"
+        placeholder="请输入搜索内容"
+        prefix-icon="el-icon-search"
+      />
+    </template>
+    <template v-slot:hright>
+      <el-button
+        type="primary"
+        icon="el-icon-plus"
+      >
+        权限
+      </el-button>
+    </template>
+    <template v-slot:body>
+      <el-table
+        :data="rolesData"
+        style="width: 100%;margin-bottom: 20px;"
+        row-key="id"
+        border
+        fit
+        highlight-current-row
+      >
+        <el-table-column
+          prop="name"
+          label="名称"
+          sortable
+          width="180"
+        />
+
+        <el-table-column
+          prop="description"
+          label="描述"
+        />
+
+        <el-table-column
+          width="120"
+          align="center"
+          label="类型"
+        >
+          <template slot-scope="{row}">
+            {{ row.type ? '自定义' : '系统' }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          width="120"
+          align="center"
+          label="操作"
+        >
+          <template slot-scope="{row}">
+            <i
+              v-if="row.type"
+              class="el-icon-edit-outline table-icon-action"
+              @click="edit(row)"
+            />
+            <i
+              v-if="row.type"
+              class="el-icon-delete table-icon-action"
+              @click="remove(row)"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+    </template>
+  </app-content>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Route } from 'vue-router';
+import { Form as ElForm, Input } from 'element-ui';
+import { UserModule } from '@/store/modules/user';
+import { isValidUsername } from '@/utils/validate';
+import { Dictionary } from 'vue-router/types/router';
+import AppContent from '@/components/Content/index.vue';
+
+@Component({
+  name: 'permissionsApp',
+  components: {
+    AppContent
+  }
+})
+export default class extends Vue {
+  private searchText = '';
+
+  private rolesData = [
+    {
+      id: 1,
+      type: 0,
+      description: '这个是管理员',
+      name: '管理员'
+    },
+    {
+      id: 2,
+      type: 0,
+      description: '这个是普通成员',
+      name: '普通成员'
+    },
+    {
+      id: 3,
+      type: 0,
+      description: '这个是只读成员',
+      name: '只读成员'
+    },
+    {
+      id: 4,
+      type: 1,
+      description: '这个是自定义权限',
+      name: '自定义权限'
+    }
+  ];
+
+  mounted() {}
+
+  edit(row: any) {}
+  remove(row: any) {}
+}
+</script>
+
+<style lang="scss">
+.permissions-app {
+  padding: 0;
+}
+</style>
