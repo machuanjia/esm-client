954a36bba78800d30fb64d35acd43eee7ce04fe1
machuanjia <yanshi0429@126.com>
2020-02-01 10:34:39 +0800

    add

--- src/views/schedule/schedule-management/schedule-calender-management.view.vue
+++ src/views/schedule/schedule-management/schedule-calender-management.view.vue
@@ -1,121 +1,206 @@
 <template>
-  <app-content
-    :is-flex-header="true"
+  <app-content-full
+    :is-hleft="true"
     :is-hright="true"
     :is-hcenter="true"
   >
-    <template v-slot:hcenter>
-      <el-input
-        v-model="searchText"
-        class="search-container"
-        placeholder="请输入搜索内容"
-        prefix-icon="el-icon-search"
-      />
+    <template v-slot:hleft>
+      <span class="calendar-full-title">
+        <svg-icon name="calendar-event" />
+        <span class="ml5">日历类型</span>
+      </span>
     </template>
-    <template v-slot:hright>
-      <el-button
-        type="primary"
-        icon="el-icon-plus"
-      >
-        日历
-      </el-button>
-    </template>
+    <template v-slot:hcenter />
+    <template v-slot:hright />
+
     <template v-slot:body>
-      <el-table
-        :data="rolesData"
-        style="width: 100%;margin-bottom: 20px;"
-        row-key="id"
-        border
-        default-expand-all
-        fit
-        highlight-current-row
-        :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+      <app-content
+        :is-flex-header="true"
+        :is-hright="true"
+        :is-hcenter="true"
       >
-        <el-table-column
-          prop="name"
-          label="名称"
-          sortable
-          width="180"
-        />
-        <el-table-column
-          prop="description"
-          label="描述"
-        />
-        <el-table-column
-          label="状态"
-          align="center"
-          width="100"
-        >
-          <template slot-scope="{row}">
-            <el-switch
-              v-model="row.status"
-              active-color="#13ce66"
-              inactive-color="#ff4949"
-              active-value="1"
-              inactive-value="0"
+        <template v-slot:hcenter>
+          <el-input
+            v-model="searchText"
+            class="search-container"
+            placeholder="请输入搜索内容"
+            prefix-icon="el-icon-search"
+          />
+        </template>
+        <template v-slot:hright>
+          <el-button
+            type="primary"
+            icon="el-icon-plus"
+            @click="openCollectionAction"
+          >
+            日历类型
+          </el-button>
+        </template>
+        <template v-slot:body>
+          <el-table
+            :data="categotyes"
+            style="width: 100%;margin-bottom: 20px;"
+            row-key="id"
+            border
+            default-expand-all
+            fit
+            highlight-current-row
+            :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+          >
+            <el-table-column
+              prop="name"
+              label="名称"
+              sortable
+              width="180"
             />
-          </template>
-        </el-table-column>
-        <el-table-column
-          width="120"
-          align="center"
-          label="操作"
-        >
-          <template slot-scope="{row}">
-            <i
-              class="el-icon-edit-outline table-icon-action"
-              @click="edit(row)"
+            <el-table-column
+              prop="description"
+              label="描述"
             />
-            <i
-              class="el-icon-delete table-icon-action"
-              @click="remove(row)"
+            <el-table-column
+              label="状态"
+              align="center"
+              width="100"
+            >
+              <template slot-scope="{row}">
+                <el-switch
+                  v-model="row.status"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949"
+                  :active-value="1"
+                  :inactive-value="0"
+                  @change="changeStatus(row)"
+                />
+              </template>
+            </el-table-column>
+            <el-table-column
+              width="120"
+              align="center"
+              label="操作"
+            >
+              <template slot-scope="{row}">
+                <i
+                  class="el-icon-edit-outline table-icon-action"
+                  @click="editAction(row)"
+                />
+                <i
+                  class="el-icon-delete table-icon-action"
+                  @click="removeAction(row)"
+                />
+              </template>
+            </el-table-column>
+          </el-table>
+          <el-dialog
+            v-if="collectionVisible"
+            :title="collectionTitle"
+            :visible.sync="collectionVisible"
+            :width="collectionSize"
+          >
+            <scheduleCalenderCollection
+              :entity="entity"
+              @saveAction="saveAction"
+              @cancelAction="cancelAction"
             />
-          </template>
-        </el-table-column>
-      </el-table>
+          </el-dialog>
+        </template>
+      </app-content>
     </template>
-  </app-content>
+  </app-content-full>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator'
-import { Route } from 'vue-router'
-import { Form as ElForm, Input } from 'element-ui'
-import { UserModule } from '@/store/modules/user'
-import { isValidUsername } from '@/utils/validate'
-import { Dictionary } from 'vue-router/types/router'
-import AppContent from '@/components/Content/index.vue'
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Route } from 'vue-router';
+import { Form as ElForm, Input } from 'element-ui';
+import { UserModule } from '@/store/modules/user';
+import { isValidUsername } from '@/utils/validate';
+import { Dictionary } from 'vue-router/types/router';
+import AppContent from '@/components/Content/index.vue';
+import AppContentFull from '@/components/Content/content-full.vue';
+import { mixins } from 'vue-class-component';
+import ViewMixin from '@/components/Mixin';
+import scheduleCalenderCollection from '@/views/schedule/schedule-management/schedule-calender-categoty-collection.vue';
+import {
+  getCalenderCategorys,
+  getCalenderCategory,
+  addCalenderCategory,
+  updateCalenderCategory,
+  deleteCalenderCategory
+} from '@/api/schedule-setting';
 
 @Component({
   name: 'calendarMangement',
   components: {
-    AppContent
+    AppContent,
+    AppContentFull,
+    scheduleCalenderCollection
   }
 })
-export default class extends Vue {
-  private searchText = ''
+export default class extends mixins(ViewMixin) {
+  private categotyes = [];
 
-  private rolesData = [{
-    id: 1,
-    description: '研发日历',
-    status: '1',
-    name: '研发日历'
-  }, {
-    id: 2,
-    description: '销售日历',
-    status: '0',
-    name: '销售日历'
-  }]
+  created() {
+    this.getCalenderCategorys();
+  }
+  mounted() {}
 
-  mounted() {
-
+  async editAction(row: any) {
+    const { data } = await getCalenderCategory(row.id, {});
+    if (data) {
+      this.editCollectionAction(data);
+    }
   }
 
-  edit(row:any) {
-
+  saveAction(payload: any) {
+    if (this.entity) {
+      this.updateCollection(this.entity.id, payload);
+    } else {
+      this.addCalenderCategory(payload);
+    }
+    this.closeCollectionaction();
   }
-  remove(row:any) {
 
+  cancelAction() {
+    this.closeCollectionaction();
+  }
+
+  removeAction(row: any) {
+    this.removeCollectionAction(async(successFn: any) => {
+      const { data } = await deleteCalenderCategory(row.id);
+      this.deleteCollection(this.categotyes, row);
+      if (successFn) {
+        successFn();
+      }
+    });
+  }
+
+  changeStatus(row: any) {
+    this.updateCalenderCategory(row.id, {
+      name: row.name,
+      status: row.status,
+      description: row.description
+    });
+  }
+
+  private async getCalenderCategorys() {
+    const { data } = await getCalenderCategorys({});
+    if (data) {
+      this.categotyes = data;
+    }
+  }
+
+  private async addCalenderCategory(payload: any) {
+    const { data } = await addCalenderCategory(payload);
+    if (data) {
+      this.pushCollection(this.categotyes, data);
+    }
+  }
+
+  private async updateCalenderCategory(id: number, payload: any) {
+    const { data } = await updateCalenderCategory(id, payload);
+    if (data) {
+      this.updateCollection(this.categotyes, data);
+    }
   }
 }
 </script>
