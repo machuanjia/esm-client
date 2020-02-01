26acab9e37c9480d30fe9448f4a0b647c4f07fb0
machuanjia <yanshi0429@126.com>
2020-02-01 21:21:20 +0800

    add

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,31 +1,33 @@
-declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+declare module "*.vue" {
+  import Vue from "vue";
+  export default Vue;
 }
 
-declare module 'element-ui/lib/locale/lang/*' {
-  export const elementLocale: any
+declare module "element-ui/lib/locale/lang/*" {
+  export const elementLocale: any;
 }
 
-declare module '*.gif' {
-  export const gif: any
+declare module "*.gif" {
+  export const gif: any;
 }
 
 // TODO: remove this part after vue-count-to has its typescript file
-declare module 'vue-count-to'
+declare module "vue-count-to";
 
 // TODO: remove this part after vuedraggable has its typescript file
-declare module 'vuedraggable'
+declare module "vuedraggable";
 
 // TODO: remove this part after vue2-dropzone has its typescript file
-declare module 'vue2-dropzone'
+declare module "vue2-dropzone";
 
 // TODO: remove this part after vue-image-crop-upload has its typescript file
-declare module 'vue-image-crop-upload'
+declare module "vue-image-crop-upload";
 
 // TODO: remove this part after vue-splitpane has its typescript file
-declare module 'vue-splitpane'
+declare module "vue-splitpane";
 
-declare module '@fullcalendar/vue'
+declare module "@fullcalendar/vue";
 
-declare module '@fullcalendar/list'
+declare module "@fullcalendar/list";
+
+declare module "vue-gantt-schedule-timeline-calendar";
