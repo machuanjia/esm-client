3cf75ba8cc66ed295e9eb4cd86b30055fc06a04a
machuanjia <yanshi0429@126.com>
2020-02-10 16:14:22 +0800

    add

--- src/views/statistics/boards/boards.view.vue
+++ src/views/statistics/boards/boards.view.vue
@@ -1,102 +1,17 @@
 <template>
-  <app-content-full
-    :is-hleft="true"
-    :is-hright="true"
-    :is-hcenter="true"
-  >
-    <template v-slot:hleft>
-      <span class="app-content-full-title">
-        <svg-icon name="tab" />
-        <span class="ml5">{{ $t('route.boards') }}</span>
-      </span>
-    </template>
-    <template v-slot:hcenter />
-    <template v-slot:hright />
-
-    <template v-slot:body>
-      <app-content
-        :is-flex-header="true"
-        :is-hright="true"
-        :is-hcenter="true"
-      >
-        <template v-slot:hcenter>
-          <el-input
-            v-model="searchText"
-            class="search-container"
-            placeholder="请输入搜索内容"
-            prefix-icon="el-icon-search"
-          />
-        </template>
-        <template v-slot:hright>
-          <el-button
-            type="primary"
-            icon="el-icon-plus"
-          >
-            看板
-          </el-button>
-        </template>
-        <template v-slot:body>
-          <el-table
-            :data="rolesData"
-            style="width: 100%;margin-bottom: 20px;"
-            row-key="id"
-            border
-            default-expand-all
-            fit
-            highlight-current-row
-            :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
-          >
-            <el-table-column
-              prop="name"
-              label="名称"
-              sortable
-              width="180"
-            />
-            <el-table-column
-              prop="description"
-              label="描述"
-            />
-            <el-table-column
-              prop="scope"
-              label="可见范围"
-              width="300"
-            />
-            <el-table-column
-              label="状态"
-              align="center"
-              width="100"
-            >
-              <template slot-scope="{row}">
-                <el-switch
-                  v-model="row.status"
-                  active-color="#13ce66"
-                  inactive-color="#ff4949"
-                  active-value="1"
-                  inactive-value="0"
-                />
-              </template>
-            </el-table-column>
-            <el-table-column
-              width="120"
-              align="center"
-              label="操作"
-            >
-              <template slot-scope="{row}">
-                <i
-                  class="el-icon-edit-outline table-icon-action"
-                  @click="edit(row)"
-                />
-                <i
-                  class="el-icon-delete table-icon-action"
-                  @click="remove(row)"
-                />
-              </template>
-            </el-table-column>
-          </el-table>
-        </template>
-      </app-content>
-    </template>
-  </app-content-full>
+  <div class="toc-layout-wrap">
+    <div class="toc-layout-toc">
+      <div class="toc-layout-toc-header">
+        <div class="toc-layout-title">
+          <svg-icon name="tab" />
+          <span>{{ $t('route.boards') }}</span>
+        </div>
+      </div>
+    </div>
+    <div class="toc-layout-main">
+      ss
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
